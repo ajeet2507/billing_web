04c8e7743a86ec674d5f3aa8c5990650fad8882a
ganesh <ganesh@zrupee.com>
2020-10-28 23:46:21 +0530

    billinghomepage,signup,login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path:'', redirectTo:'Onlinebilling',pathMatch:'full' },
+  { path:'Onlinebilling',component:HomeComponent, data: { title: 'Home' } },
+  { path:'signup',component:SignupComponent, data: { title: 'Signup' } },
+  { path:'login',component:LoginComponent, data: { title: 'Login' } },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    SignupComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
